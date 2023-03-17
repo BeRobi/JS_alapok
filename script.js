d78ea841e54151634d2b48455f581bfac9460cd1
BeRobi <screengsm@gmail.com>
2023-03-17 17:47:43 +0100

    1.) h2-es cím kiírása a konzolra

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 window.addEventListener("load", function () {
-    elemekElerese1();
-    elemekElerese2();
-    elemekElerese3();
+  elemekElerese1();
+  elemekElerese2();
+  elemekElerese3();
 });
 
 function elemekElerese1() {
-    /**Ide jön az első feladat */
+  const ELEM = document.getElementById("f1");
+  console.log(ELEM.innerHTML);
 }
-function elemekElerese1() {
-    /**Ide jön az 2.  feladat */
+
+function elemekElerese2() {
+  /**Ide jön az 2. feladat */
 }
-function elemekElerese1() {
-    /**Ide jön az 3. feladat */
+
+function elemekElerese3() {
+  /**Ide jön az 3. feladat */
 }
