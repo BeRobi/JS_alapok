4c69ef0bee3cb5b8ca91c1de81c3912eca018bc7
BeRobi <screengsm@gmail.com>
2023-03-17 19:50:05 +0100

    4.) véletlen számok generálása

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function () {
   elemekElerese1();
   elemekElerese2();
   elemekElerese3();
+  elemekElerese4();
 });
 
 function elemekElerese1() {
@@ -19,3 +20,17 @@ function elemekElerese3() {
   const ELEM = document.getElementsByClassName("ide")[0];
   ELEM.innerHTML += "<p>Jó reggelt!</p>";
 }
+
+function elemekElerese4() {
+  var velSzamok = new Array();
+  for (let i = 0; i < 5; i++) {
+    let velSzam = Math.floor(Math.random() * 21) + 10;
+    console.log(velSzam);
+    velSzamok[i] = velSzam;
+  }
+  const LISTA = document.getElementsByClassName("lista")[0];
+  for (let i = 0; i < velSzamok.length; i++) {
+    LISTA.innerHTML += `<p>${velSzamok[i]}</p>`;
+  }
+}
+
